d44b623cfb4ac61e68716aadad853bf5bc91d6f4
Kirk Kaiser <kirk@zothcorp.com>
2014-08-20 12:08:55 -0400

    got our working output, but you needa to hacka

--- shaderTest.cpp
+++ shaderTest.cpp
@@ -22,7 +22,12 @@ class ImageFileTestApp : public AppBasic {
 	float			mAngle;
     int outpuddded;
     fs::path outty;
+    fs::path inny;
     std::vector<std::string> texturez;
+    std::vector<int> indexez;
+    std::vector<int> indexez24p;
+    std::vector<std::string> reddy2render;
+    int counter;
 };
 
 
@@ -47,18 +52,57 @@ void ImageFileTestApp::setup()
 	}
     
     std::cout << "Iterating over directory: /Users/kirkkaiser/Desktop/DepthKit_006_OSX/video_shoot/TAKE_08_16_14_46_31/depth" << std::endl;
-    fs::path p("/Users/kirkkaiser/Desktop/DepthKit_006_OSX/video_shoot/TAKE_08_16_14_46_31/depth");
+    
+    inny = fs::path("/Users/kirkkaiser/Desktop/DepthKit_006_OSX/video_shoot/TAKE_08_16_14_46_31/depth/");
     outty = fs::path("/Users/kirkkaiser/Desktop/DepthKit_006_OSX/video_shoot/TAKE_08_16_14_46_31/render/");
-    for( fs::directory_iterator it( p ); it != fs::directory_iterator(); ++it ) {
+    
+    for( fs::directory_iterator it( inny ); it != fs::directory_iterator(); ++it ) {
         if( ! is_directory( *it ) ) {
             if (it->path().filename() == ".DS_Store") continue;
             console() << "   " << it->path().filename() << " size(bytes): " <<                                   fs::file_size( *it ) << std::endl;
             texturez.push_back(it->path().filename().string());
         }
     }
-	
+    
+    for (int i = 0; i < texturez.size(); i++) {
+        // make an array of the integer values of milliseconds into an array
+        // so we can get back down to 24p homie
+        std::string filename = texturez[i];
+        int start = filename.find("millis_") + 7;
+
+        std::string daMillis = filename.substr(start, 10);
+        int daNum = std::stoi(daMillis);
+        console() << daNum << std::endl;
+        indexez.push_back(daNum);
+    }
+    
+    float idealInterval = 41.6666; // SO CLOSE TO 42! interval in millis between frames for target framerate 1000 / 24
+    int lastFrameUsed = 0;
+    int frame = -1;
+    
+    // algo below to sort to as close to 24 p as possible
+	while (lastFrameUsed < indexez.size()) {
+        frame++;
+        int millis = frame * idealInterval;
+        while (millis > indexez[lastFrameUsed]) {
+            lastFrameUsed++;
+            if (lastFrameUsed >= indexez.size()) break;
+        }
+        indexez24p.push_back(lastFrameUsed);
+    }
+
+    for (int i =0; i < indexez24p.size(); i++) {
+        reddy2render.push_back(texturez[indexez24p[i]]); // populate our array with strings of proper filenames
+    }
+    
+    /* verify that we've got the right stuff
+    for (int i = 0; i < reddy2render.size(); i++) {
+        console() << reddy2render[i] << std::endl;
+    } */
+    
 	mAngle = 0.0f;
     outpuddded = 0;
+    counter = 1;
 }
 
 void ImageFileTestApp::keyDown( KeyEvent event )
@@ -70,7 +114,10 @@ void ImageFileTestApp::keyDown( KeyEvent event )
 
 void ImageFileTestApp::update()
 {
-
+    if (outpuddded < reddy2render.size()) {
+        mTexture = gl::Texture::create( loadImage( inny.string() + reddy2render[counter - 1] ) );
+        counter++;
+    }
 }
 
 void ImageFileTestApp::draw()
@@ -83,11 +130,11 @@ void ImageFileTestApp::draw()
 	gl::drawSolidRect( getWindowBounds() );
 
 	mTexture->unbind();
-    if (outpuddded == 0)
+    if (outpuddded < reddy2render.size())
         {
-            std::string yep = outty.string() + std::string("outty.png");
+            std::string yep = outty.string() + std::string("output") + std::to_string(counter) + std::string(".png");
             writeImage(yep, copyWindowSurface());
-            outpuddded = 1;
+            outpuddded++;
         }
 }
 
